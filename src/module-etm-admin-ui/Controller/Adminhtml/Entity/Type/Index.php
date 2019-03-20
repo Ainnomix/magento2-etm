019f0e7658b2f80f3be6d6e2eac1809f5f7f3f08
Roman TOMCHAK <romantomchak@gmail.com>
2019-03-20 12:09:18 +0200

    New modules structure. Dropped support of Magento 2.2

--- src/module-etm-admin-ui/Controller/Adminhtml/Entity/Type/Index.php
+++ src/module-etm-admin-ui/Controller/Adminhtml/Entity/Type/Index.php
@@ -5,32 +5,39 @@ declare(strict_types=1);
  * Do not edit or add to this file if you wish to upgrade Entity Type Manager to newer
  * versions in the future.
  *
- * @category  Ainnomix_EtmAdminhtml
- * @package   Ainnomix\EtmAdminhtml
+ * @category  Ainnomix_Etm
+ * @package   Ainnomix\EtmAdminUi
  * @author    Roman Tomchak <romantomchak@gmail.com>
- * @copyright 2018 Ainnomix
+ * @copyright 2019 Ainnomix
  * @license   Open Software License ("OSL") v. 3.0
  */
 
-namespace Ainnomix\EtmAdminhtml\Controller\Adminhtml\Entity\Type;
+namespace Ainnomix\EtmAdminUi\Controller\Adminhtml\Entity\Type;
 
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
 
 /**
  * Entity type list action class
  *
- * @category Ainnomix_EtmAdminhtml
- * @package  Ainnomix\EtmAdminhtml
+ * @category Ainnomix_Etm
+ * @package  Ainnomix\EtmAdminUi
  * @author   Roman Tomchak <romantomchak@ainnomix.com>
  */
-class Index extends \Magento\Backend\App\Action
+class Index extends \Magento\Backend\App\Action implements HttpGetActionInterface
 {
 
+    /**
+     * Execute controller action
+     *
+     * @return ResultInterface
+     */
     public function execute()
     {
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
 
-        $resultPage->setActiveMenu('Ainnomix_EtmAdminhtml::management_index');
+        $resultPage->setActiveMenu('Ainnomix_EtmAdminUi::management_index');
 
         $resultPage->getConfig()->getTitle()->prepend(__('Entity Type Manager'));
         $resultPage->getConfig()->getTitle()->prepend(__('Manage Entity Types'));
--- src/module-etm-admin-ui/registration.php
+++ src/module-etm-admin-ui/registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Ainnomix_EtmStaging',
+    'Ainnomix_EtmAdminUi',
     __DIR__
 );
